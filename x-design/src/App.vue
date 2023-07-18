3e67ca8e3e36db40cd35e5278a6ed9e3f2982cf8
xiaoshenghui <1653618993@qq.com>
2023-07-18 09:59:17 +0800

    优化组件文档布局Layout

--- x-design/src/App.vue
+++ x-design/src/App.vue
@@ -1,34 +1,52 @@
 <template>
   <div id="app">
-    <nav>
+    <nav class="header-app">
       <router-link to="/">主页</router-link> |
       <router-link to="/test">测试页</router-link> |
       <router-link to="/user">组件文档</router-link>
     </nav>
-    <router-view />
+    <div class="main-app">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  overflow: hidden;
 }
 
 nav {
   padding: 30px;
+  border-bottom: 1px solid #eee;
+  position: fixed;
+  width: 100%;
+  background-color: #fff;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    margin: 0 20px;
+    font-size: 20px;
 
     &.router-link-exact-active {
       color: rgb(251, 114, 153);
     }
   }
+}
+
+.main-app {
+  margin-top: 6.5%;
 }
 
 .icon {
--- x-design/src/views/UserView.vue
+++ x-design/src/views/UserView.vue
@@ -1,16 +1,105 @@
 <template>
     <div class="userview">
-        <h1>使用文档初始化</h1>
-        <div class="installBox">
-            <h3>安装</h3>
-            <h4>快速上手</h4>
-            <h5>引入 Xdesign</h5>
-            <span>在 main.js 中写入以下内容：</span>
-            <pre>
+        <div class="left-user">
+            <ul>
+                <li>
+                    <a>开发指南</a>
+                    <a href="#coding-instal">安装</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>开发指南</a>
+                    <a href="#coding-instal">安装</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+                <li>
+                    <a>组件</a>
+                    <a href="#coding-button3">按钮3</a>
+                </li>
+            </ul>
+        </div>
+        <div class="right-user">
+            <h1>使用文档初始化</h1>
+            <div class="installBox">
+                <h3 id="coding-instal">安装</h3>
+                <h4>快速上手</h4>
+                <h5>引入 Xdesign</h5>
+                <span>在 main.js 中写入以下内容：</span>
+                <pre>
 import '@/assets/aliIcon/iconfont'
 import XDesign from './xdesign'
 Vue.use(XDesign)
-            </pre>
+                </pre>
+                <h3 id="coding-button">按钮</h3>
+                <h4>快速上手</h4>
+                <h5>引入 Xdesign</h5>
+                <span>在 main.js 中写入以下内容：</span>
+                <pre>
+import '@/assets/aliIcon/iconfont'
+import XDesign from './xdesign'
+Vue.use(XDesign)
+                </pre>
+                <h3 id="coding-button2">按钮2</h3>
+                <h4>快速上手</h4>
+                <h5>引入 Xdesign</h5>
+                <span>在 main.js 中写入以下内容：</span>
+                <pre>
+import '@/assets/aliIcon/iconfont'
+import XDesign from './xdesign'
+Vue.use(XDesign)
+                </pre>
+                <h3 id="coding-button3">按钮3</h3>
+                <h4>快速上手</h4>
+                <h5>引入 Xdesign</h5>
+                <span>在 main.js 中写入以下内容：</span>
+                <pre>
+import '@/assets/aliIcon/iconfont'
+import XDesign from './xdesign'
+Vue.use(XDesign)
+                </pre>
+            </div>
         </div>
     </div>
 </template>
@@ -21,8 +110,47 @@ export default {
 </script>
 <style lang="less" scoped>
 .userview {
-    padding: 0 30px;
-    text-align: left;
+    overflow: hidden;
+
+    .left-user {
+        width: 18%;
+        height: 100%;
+        border-right: 1px solid #eee;
+        text-align: left;
+        padding: 10px;
+        overflow-y: scroll;
+        scroll-behavior: smooth;
+        position: fixed;
+
+        ul {
+            list-style: none;
+
+            li {
+                a {
+                    display: block;
+                    margin: 10px 0;
+                    color: #888;
+                    cursor: pointer;
+                    text-decoration: none;
+                }
+
+                a:first-child {
+                    font-weight: bold;
+                    color: #333;
+                }
+            }
+        }
+    }
+
+
+    .right-user {
+        width: 76.5%;
+        border: 1px solid #eee;
+        text-align: left;
+        margin: 10px auto;
+        margin-left: 20%;
+        padding: 20px;
+    }
 }
 
 pre {
