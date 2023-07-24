2649b044c645e9062e3b13158e720504e315661b
xiaoshenghui <1653618993@qq.com>
2023-07-24 20:33:53 +0800

    体验一下原生js实现级联选择器

---
+++ blog/级联选择器/data.js
@@ -0,0 +1,145 @@
+// 所有的数据
+var data = [
+	{
+		id: 0,
+		value: "江西省",
+		children: [
+			{
+				id: 0,
+				value: "赣州市",
+				children: [
+					{
+						id: 0,
+						value: '于都县'
+					},
+					{
+						id: 1,
+						value: '宁都县'
+					},
+					{
+						id: 2,
+						value: '赣县'
+					}
+				]
+			},
+			{
+				id: 1,
+				value: "九江市",
+				children: [
+					{
+						id: 0,
+						value: '武宁县'
+					},
+					{
+						id: 1,
+						value: '修水县'
+					},
+					{
+						id: 2,
+						value: '永修县'
+					}
+				]
+			}
+		]
+	},
+	{
+		id: 1,
+		value: "河南省",
+		children: [
+			{
+				id: 0,
+				value: "郑州市",
+				children: [
+					{
+						id: 0,
+						value: '中牟县'
+					},
+					{
+						id: 1,
+						value: '中原区'
+					},
+					{
+						id: 2,
+						value: '二七区'
+					}
+				]
+			},
+			{
+				id: 1,
+				value: "开封市",
+				children: [
+					{
+						id: 0,
+						value: '杞县'
+					},
+					{
+						id: 1,
+						value: '通许县'
+					},
+					{
+						id: 2,
+						value: '尉氏县'
+					},
+					{
+						id: 3,
+						value: '兰考县'
+					}
+				]
+			}
+		]
+	},
+	{
+		id: 2,
+		value: "广东省",
+		children: [
+			{
+				id: 0,
+				value: "深圳市",
+				children: [
+					{
+						id: 0,
+						value: '罗湖区'
+					},
+					{
+						id: 1,
+						value: '福田区'
+					},
+					{
+						id: 2,
+						value: '南山区'
+					},
+					{
+						id: 3,
+						value: '宝安区'
+					},
+					{
+						id: 4,
+						value: '龙岗区'
+					}
+				]
+			},
+			{
+				id: 1,
+				value: "广州市",
+				children: [
+					{
+						id: 0,
+						value: '荔湾区'
+					},
+					{
+						id: 1,
+						value: '越秀区'
+					},
+					{
+						id: 2,
+						value: '海珠区'
+					},
+					{
+						id: 3,
+						value: '天河区'
+					}
+				]
+			}
+		]
+	}
+];
